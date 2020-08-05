365f8eabafa774d6759523e4bb124f5e7c3c67da
dwight9339 <whited9339@gmail.com>
2020-08-05 17:41:35 -0600

    Create names.js

---
+++ names.js
@@ -0,0 +1,14 @@
+module.exports = {
+    dog: {
+        male: ["Jerry", "Fido", "Luke"],
+        female: ["Sue", "Lady Bird", "Annabelle"]
+    },
+    cat: {
+        male: ["Tom", "Felix", "Pepe"],
+        female: ["Sally", "Jinx", "Bella"]
+    },
+    other: {
+        male: ["Harry"],
+        female: ["Cindy"]
+    }
+}
